0157baf8f7a8606468e59fd0a68e2fce1c0e123e
Roderick Shoolbraid <rdxenv@gmail.com>
2025-04-08 17:35:51 -0700

    glasscity added 60%

---
+++ dl/glassCity-script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const logoLink = document.getElementById('charlotta-logo-link');
+  if (logoLink) {
+    logoLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.open('../index.html', '_blank', 'noopener,noreferrer,width=1280,height=800');
+    });
+  }
+
+
+  
+  const downloadLink = document.getElementById('download-zip');
+  if (downloadLink) {
+    downloadLink.addEventListener('click', function (e) {
+      const confirmed = confirm(
+        "This file is ~982.8 MB.\n\nAre you sure you want to download Glass City of Us.zip?"
+      );
+      if (!confirmed) {
+        e.preventDefault();
+      }
+    });
+  }
+});
